45bbb843f2ff1b37c96264cb0be98b6129ebbfc7
pabloipl <101835474+pabloipl@users.noreply.github.com>
2022-03-23 10:07:12 -0300

    Add files via upload

--- Carrito.java
+++ Carrito.java
@@ -4,21 +4,34 @@ public class Carrito {
 
     public static void main(String[] args){
 
+        
+
     }
 
     public static int[] generarCarrito(int[]carro){
-
+        int p;
         int n;
         n=(int)(Math.random()*20)+1;
         carro=new int[n];
 
+        int i=0;
+        if(carro[i]<n+2){
+            p=(int)(Math.random()*15)+1;
+            carro[i]=p;
+            i++;
+
+        }
+
         return carro;
+    }
+
+    public static int leerCantidad(int productos){
+
+
+
+
 
     }
 
-    public static int agregarProductos(int p){
-
-        
-    }
 
 }
