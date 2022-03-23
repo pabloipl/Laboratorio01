f3cd7e5d3f0b3e72509d539d2859503bc27412ca
pabloipl <101835474+pabloipl@users.noreply.github.com>
2022-03-23 09:35:37 -0300

    Add files via upload

---
+++ Carrito.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Carrito {
+
+    public static void main(String[] args){
+
+    }
+
+    public static int[] generarCarrito(int[]carro){
+
+        int n;
+        n=(int)(Math.random()*20)+1;
+        carro=new int[n];
+
+        return carro;
+
+    }
+
+    public static int agregarProductos(int p){
+
+        
+    }
+
+}
